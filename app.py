3cdbc0419f49cfc61a279073cda82233df13f309
Hamza Malik <45312947+thehamzza@users.noreply.github.com>
2022-07-18 02:08:07 +0500

    changed back to the original version
    that was running on the local host pc machine

--- app.py
+++ app.py
@@ -3,30 +3,13 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import plotly.express as px
-import pymongo
 
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-# @st.experimental_singleton(suppress_st_warning=True)
-# def init_connection():
-#     return pymongo.MongoClient(**st.secrets["mongo"], connect=False)
 
-# client = init_connection()
 
-# Pull data from the collection.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-# @st.experimental_memo(ttl=600)
-# def get_data():
-#     db = client.mydb
-#     items = db.mycollection.find()
-#     items = list(items)  # make hashable for st.experimental_memo
-#     return items
-#
-# items = get_data()
 
-#DATA_URL = "G:/WORK/GRAY_SOLUTIONS_PROJECTS/Sreamlit_Project/Motor_Vehicle_Collisions_Crashes.csv"
+DATA_URL = "G:/WORK/GRAY_SOLUTIONS_PROJECTS/Sreamlit_Project/Motor_Vehicle_Collisions_Crashes.csv"
 #DATA_URL = "https://github.com/thehamzza/Streamlit-Web-App-Motor-Vehicle-Collisions-in-New-York-City/blob/main/Motor_Vehicle_Collisions_Crashes.csv"
-DATA_URL = ("https://github.com/thehamzza/Streamlit-Web-App-Motor-Vehicle-Collisions-in-New-York-City/blob/77f82403f9a58fc252a984cc8272730dcdc46af8/Motor_Vehicle_Collisions_Crashes.csv")
+#DATA_URL = ("https://github.com/thehamzza/Streamlit-Web-App-Motor-Vehicle-Collisions-in-New-York-City/blob/77f82403f9a58fc252a984cc8272730dcdc46af8/Motor_Vehicle_Collisions_Crashes.csv")
 
 
 st.title("Motor Vehicle Collisions in New York City")
@@ -34,44 +17,19 @@ st.markdown("This application is a Streamlit dashboard that can be used to analy
 
 # Pull data from the collection.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-# @st.experimental_memo(ttl=600)
-##@st.cache(persist=True)
-# def load_data(nrows):
-#     db = client.mydb
-#     items = db.mycollection.find()
-#     items = list(items)  # make hashable for st.experimental_memo
 
-#     #data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-#     data = pd.DataFrame(items, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-#     data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
-#     lowercase= lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data.rename(columns={'crash_date_crash_time':'date/time'}, inplace=True)
-#     return data
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    #data = pd.DataFrame(items, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+    lowercase= lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash_date_crash_time':'date/time'}, inplace=True)
+    return data
 
-def get_database():
-    from pymongo import MongoClient
-    import pymongo
+data = load_data(100000)
 
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb://<username>:<password>@<cluster0>.mongodb.net/motor_vehicle_crash_nyc.motor_vehicle_crash_data"
-
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
-
-    # Create the database for our example (we will use the same database throughout the tutorial
-    return client
-
-data = get_database()
-
-#data = load_data(100000)
-#data = get_data
-data = pd.DataFrame(data)
-data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
-lowercase= lambda x: str(x).lower()
-data.rename(lowercase, axis='columns', inplace=True)
-data.rename(columns={'crash_date_crash_time':'date/time'}, inplace=True)
-#return data
 
 st.header("Where are the most people injured in NYC")
 injured_people = st.slider("Number of persons injured in vehicle collision", 0, 19)
